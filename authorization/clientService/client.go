d53c07d82b6b7c26828795a495439f89c09f938a
Arakasi21 <40834324+Arakasi21@users.noreply.github.com>
2023-06-14 20:39:08 +0600

    sad

--- authorization/clientService/client.go
+++ authorization/clientService/client.go
@@ -46,7 +46,7 @@ func main() {
 	for {
 		var command string
 		commandPrompt := &survey.Input{
-			Message: "Enter a command ('getProfile' or 'otherCommand'), or 'exit' to quit:",
+			Message: "Enter a command ('getProfile', 'createCar', 'rentCar', or 'exit' to quit):",
 		}
 		err = survey.AskOne(commandPrompt, &command)
 		if err != nil {
@@ -59,6 +59,16 @@ func main() {
 			err = getUserProfile(client, userID)
 			if err != nil {
 				log.Fatalf("Failed to get user profile: %v", err)
+			}
+		} else if command == "createCar" {
+			err = createCar(client, userID)
+			if err != nil {
+				log.Fatalf("Failed to create car: %v", err)
+			}
+		} else if command == "rentCar" {
+			err = rentCar(client, userID)
+			if err != nil {
+				log.Fatalf("Failed to rent car: %v", err)
 			}
 		} else {
 			fmt.Println("Invalid command.")
@@ -161,6 +171,64 @@ func getUserProfile(client pb.UserServiceClient, userID int32) error {
 	fmt.Printf("Name: %s\n", response.Name)
 	fmt.Printf("Surname: %s\n", response.Surname)
 	fmt.Printf("Email: %s\n", response.Email)
+
+	return nil
+}
+
+func createCar(client pb.UserServiceClient, userID int32) error {
+	var createCarRequest pb.CreateCarRequest
+	err := survey.Ask([]*survey.Question{
+		{
+			Name: "brand",
+			Prompt: &survey.Input{
+				Message: "Enter the car brand:",
+			},
+		},
+		{
+			Name: "description",
+			Prompt: &survey.Input{
+				Message: "Enter the car description:",
+			},
+		},
+	}, &createCarRequest)
+	if err != nil {
+		return fmt.Errorf("failed to get car details: %v", err)
+	}
+
+	createCarRequest.OwnerId = userID
+
+	response, err := client.CreateCar(context.Background(), &createCarRequest)
+	if err != nil {
+		return fmt.Errorf("failed to create car: %v", err)
+	}
+
+	fmt.Printf("Car created successfully! Car ID: %d\n", response.CarId)
+
+	return nil
+}
+
+func rentCar(client pb.UserServiceClient, userID int32) error {
+	var rentCarRequest pb.RentCarRequest
+	err := survey.Ask([]*survey.Question{
+		{
+			Name: "carId",
+			Prompt: &survey.Input{
+				Message: "Enter the ID of the car you want to rent:",
+			},
+		},
+	}, &rentCarRequest)
+	if err != nil {
+		return fmt.Errorf("failed to get car ID: %v", err)
+	}
+
+	rentCarRequest.UserId = userID
+
+	response, err := client.RentCar(context.Background(), &rentCarRequest)
+	if err != nil {
+		return fmt.Errorf("failed to rent car: %v", err)
+	}
+
+	fmt.Printf("Car rented successfully! Rental ID: %d\n", response.CarId)
 
 	return nil
 }
